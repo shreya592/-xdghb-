4bdd811a2c84fc1ad0d4dfcfd9588ad11bbbd090
shreya592 <80126503+shreya592@users.noreply.github.com>
2021-08-08 15:04:28 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas=createCanvas(640, 480);
+    canvas.position(110, 250);
+    video= createCapture(VIDEO);
+    video.hide();
+
+}
+
+function draw()
+{
+    image(video, 0, 0, 640, 480);
+}
+
+function take_snapshot()
+{
+    save("snapshot.png");
+}
